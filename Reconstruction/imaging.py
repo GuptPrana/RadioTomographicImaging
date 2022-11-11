82a2ab5e4262f84d1d80492a1836222787c473de
Pranav Gupta <68948549+GuptPrana@users.noreply.github.com>
2022-11-12 02:51:40 +0800

    Add files via upload

--- Reconstruction/imaging.py
+++ Reconstruction/imaging.py
@@ -4,23 +4,24 @@ import nodeLinks as nl
 import reconstructors
 from matplotlib import pyplot as plt
 
-def generateImage(weightsFile='defineWeights.json', RSSI=[], resolution=32):
+def generateImage(weightsFile='defineWeights.json', RSSI=[], resolution=33):
     weights = json.load(open(weightsFile))
     weights = np.array(weights)
-    print(weights.shape)
-
-    w = np.array(weights[0])
-    w = np.split(w, resolution)
-    print(len(w))
-    print(len(w[0]))
-    plt.imshow(w)
-    plt.show()
+    # print(weights.shape)
     '''
+    # replace np.ones([130, 1]) with the RSSI values of same dimension
     image = reconstructors.reconstructSVD(weights, np.ones([130, 1]))
     image = np.split(image, resolution)
     plt.imshow(image)
     plt.show()
+    return
     '''
+    # The below is only for demo and can be removed
+    # Change i in weights[i] to see different lines
+    w = np.array(weights[-1])
+    w = np.split(w, resolution)
+    plt.imshow(w)
+    plt.show()
     return
     
 generateImage()
