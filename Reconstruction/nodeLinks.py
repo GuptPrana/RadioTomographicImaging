ba822f74096e08ff0b6586bf02d517a0209ae144
Pranav Gupta <68948549+GuptPrana@users.noreply.github.com>
2023-02-07 21:21:56 +0800

    Added Ellipsoid Method
    Better than Bresenham

--- Reconstruction/nodeLinks.py
+++ Reconstruction/nodeLinks.py
@@ -1,12 +1,12 @@
 import json
 import numpy as np
+import math 
 
 # Define the Coordinate Plane
-# Assume Rectangular Arrangment of Nodes
 # Assume Wavelength = 12.5cm. Each square in grid can be delta = 0.5*12.5 = 6.25cm wide.
-def generateGrid(delta=6.25,nodeFile='defineNodes.json',corners=0):
+def generateGrid(delta=6.25,nodeFile='defineNodes.json',corners=0,ROI='e'):
     nodes = json.load(open(nodeFile))
-    
+    # Identify the corners in map
     minX, maxX = 0, 0
     minY, maxY = 0, 0
     for node in nodes['nodeList']:
@@ -18,25 +18,32 @@ def generateGrid(delta=6.25,nodeFile='defineNodes.json',corners=0):
             maxY = node[1]
         elif (node[1]<minY):
             minY = node[1]
-
-    # Key: 0 to 32 is 33 squares, NOT 32
-    height = int((maxY-minY)/delta)+((maxY-minY)%delta>0)+1
-    width = int((maxX-minX)/delta)+((maxX-minX)%delta>0)+1
-    grid = np.zeros((width, height))
-    # axisX = np.linspace(minX, maxX, num=int(width/delta)+(width%delta>0), endpoint=True)
-    # axisY = np.linspace(minY, maxY, num=int(height/delta)+(height%delta>0), endpoint=True)
-    # gridX, gridY = np.meshgrid(axisX, axisY)
-    if (corners==1):
-        minX = int(minX/delta)+(minX%delta>0)
-        maxX = int(maxX/delta)+(maxX%delta>0)
-        minY = int(minY/delta)+(minY%delta>0)
-        maxY = int(maxY/delta)+(maxY%delta>0)
-        return grid, minX, maxX, minY, maxY
-    return grid
+    
+    # ROI = 'b': Bresenham, 'e': ellipse
+    if (ROI=='b'):
+        grid = np.zeros((width, height))
+        height = int((maxY-minY)/delta)+((maxY-minY)%delta>0)+1
+        width = int((maxX-minX)/delta)+((maxX-minX)%delta>0)+1
+        if (corners==1):
+            minX = int(minX/delta)+(minX%delta>0)
+            maxX = int(maxX/delta)+(maxX%delta>0)
+            minY = int(minY/delta)+(minY%delta>0)
+            maxY = int(maxY/delta)+(maxY%delta>0)
+            return grid, minX, maxX, minY, maxY
+        return grid
+    else: 
+        # 200/6.25 = 32 squares in the grid
+        # height = int((maxY-minY)/delta)+((maxY-minY)%delta>0)
+        # width = int((maxX-minX)/delta)+((maxX-minX)%delta>0)
+        maxX = int((maxY-minY)/delta)+((maxY-minY)%delta>0)
+        maxY = int((maxX-minX)/delta)+((maxX-minX)%delta>0)
+        minX = 0
+        minY = 0
+        return maxX, maxY
 
 # Use Bresenham's Line Algorithm to Generate Coordinates in Line of Sight
 # Assume No Scattering
-def generateWeights(thisGrid, pointA, pointB):
+def generateWeightsBres(thisGrid, pointA, pointB):
     x1, y1 = pointA
     x2, y2 = pointB
     # Edge Cases
@@ -47,7 +54,8 @@ def generateWeights(thisGrid, pointA, pointB):
         x1, x2 = x2, x1
         y1, y2 = y2, y1
     # distance for weight
-    w = 1/np.sqrt((x2-x1)**2+(y2-y1)**2)
+    # w = 1/np.sqrt((x2-x1)**2+(y2-y1)**2)
+    w = 1
     # Line Drawing Conditions
     slope = (y2-y1)/(x2-x1)
     ax, ay = x1, y1
@@ -79,24 +87,24 @@ def generateWeights(thisGrid, pointA, pointB):
         # Update 
         if (slope<0):
             if (abs(slope)>1):
-                thisGrid[by-y, ax+step] = 1
+                thisGrid[by-y, ax+step] = w
             else:
-                thisGrid[ax+step, by-y] = 1
+                thisGrid[ax+step, by-y] = w
         else:
             if (abs(slope)>1):
-                thisGrid[ay+y, ax+step] = 1
+                thisGrid[ay+y, ax+step] = w
             else:
-                thisGrid[ax+step, ay+y] = 1
+                thisGrid[ax+step, ay+y] = w
 
     return thisGrid
 
 # Save the Weight Matrices into JSON Dictionary
 def generateAllWeights(nodeFile='defineNodes.json', linksFile='defineLinks.json', weightsFile='defineWeights.json', delta=6.25):
     # Grid Template
-    grid, minX, maxX, minY, maxY = generateGrid(corners=1)
+    # grid, minX, maxX, minY, maxY = generateGrid(corners=1, ROI='b')
+    maxX, maxY = generateGrid()
     nodes = json.load(open(nodeFile))
     links = {}
-    # possibleLinks = {}
     linkCount = 0 
     for thisNode in nodes['nodeList']:
         # Identify possible links
@@ -113,14 +121,14 @@ def generateAllWeights(nodeFile='defineNodes.json', linksFile='defineLinks.json'
             
             # Edge Cases
             if (node[0]==otherNode[0]):
-                if (node[0]==maxY or node[0]==minY):
+                if (node[0]==maxY or node[0]==0):
                     # Same Edge
                     continue
                 elif (node[1]==otherNode[1]): 
                     # Same Node
                     continue
             elif (node[1]==otherNode[1]):
-                if (node[1]==maxX or node[1]==minX):
+                if (node[1]==maxX or node[1]==0):
                     # Same Edge
                     continue
             elif (otherNode in possibleLinks):
@@ -132,11 +140,12 @@ def generateAllWeights(nodeFile='defineNodes.json', linksFile='defineLinks.json'
                 possibleLinks.append(otherNode)
             else: 
                 continue
-            print(node)
-            print(otherNode)
+
             # Generate Weights for this Link
+
             # Can use pointer/reference to avoid sending grid in every function call
-            weight = generateWeights(np.zeros_like(grid), node, otherNode)
+            # weight = generateWeightsBres(np.zeros_like(grid), node, otherNode)
+            weight = generateWeights(node, otherNode, maxX, maxY)
             # x*y array --> 1*xy array (concatenate all y|x for all x)
             weightVector = weight.flatten()
             # concatenate this weight array into the weightMatrix --> numLinks*xy array
@@ -159,6 +168,25 @@ def generateAllWeights(nodeFile='defineNodes.json', linksFile='defineLinks.json'
         output.write(json.dumps(weightMatrix.tolist(), indent=4))
 
     return 1
+
+# Original method to determine pixels in the LOS between 2 nodes
+# if d(a,c)+d(b,c) < d(a,b) + lambda | c:center of pixel, a,b: nodes
+def generateWeights(pointA, pointB, maxX, maxY):
+    x1, y1 = pointA
+    x2, y2 = pointB
+    dist = math.sqrt((x2-x1)**2+(y2-y1)**2)
+    lamb = 0
+    dist += lamb
+    # Fixed lambda vs Changing Lambda according to size
+
+    mesh = np.mgrid[0:maxX,0:maxY]
+    distX = mesh[0]
+    distY = mesh[1]
+    distA = math.sqrt((distX - x1)**2 + (distY - y1)**2)
+    distB = math.sqrt((distX - x2)**2 + (distY - y2)**2)
+    
+    grid = (distA+distB)>dist
+    return grid
 
 ##############################################################
 # To Test: 
