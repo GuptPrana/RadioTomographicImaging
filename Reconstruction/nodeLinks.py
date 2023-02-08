f4ac9da8d3b8f7a96d96d77121696958890db1a4
Pranav Gupta <68948549+GuptPrana@users.noreply.github.com>
2023-02-08 17:01:24 +0800

    Add files via upload

--- Reconstruction/nodeLinks.py
+++ Reconstruction/nodeLinks.py
@@ -1,6 +1,5 @@
 import json
 import numpy as np
-import math 
 
 # Define the Coordinate Plane
 # Assume Wavelength = 12.5cm. Each square in grid can be delta = 0.5*12.5 = 6.25cm wide.
@@ -18,7 +17,7 @@ def generateGrid(delta=6.25,nodeFile='defineNodes.json',corners=0,ROI='e'):
             maxY = node[1]
         elif (node[1]<minY):
             minY = node[1]
-    
+
     # ROI = 'b': Bresenham, 'e': ellipse
     if (ROI=='b'):
         grid = np.zeros((width, height))
@@ -35,10 +34,9 @@ def generateGrid(delta=6.25,nodeFile='defineNodes.json',corners=0,ROI='e'):
         # 200/6.25 = 32 squares in the grid
         # height = int((maxY-minY)/delta)+((maxY-minY)%delta>0)
         # width = int((maxX-minX)/delta)+((maxX-minX)%delta>0)
-        maxX = int((maxY-minY)/delta)+((maxY-minY)%delta>0)
-        maxY = int((maxX-minX)/delta)+((maxX-minX)%delta>0)
-        minX = 0
-        minY = 0
+        # minX, minY = 0, 0
+        maxX = int((maxX-minX)/delta)+((maxX-minX)%delta>0)
+        maxY = int((maxY-minY)/delta)+((maxY-minY)%delta>0)
         return maxX, maxY
 
 # Use Bresenham's Line Algorithm to Generate Coordinates in Line of Sight
@@ -99,7 +97,7 @@ def generateWeightsBres(thisGrid, pointA, pointB):
     return thisGrid
 
 # Save the Weight Matrices into JSON Dictionary
-def generateAllWeights(nodeFile='defineNodes.json', linksFile='defineLinks.json', weightsFile='defineWeights.json', delta=6.25):
+def generateAllWeightsBres(nodeFile='defineNodes.json', linksFile='defineLinks.json', weightsFile='defineWeightsEll.json', delta=6.25):
     # Grid Template
     # grid, minX, maxX, minY, maxY = generateGrid(corners=1, ROI='b')
     maxX, maxY = generateGrid()
@@ -174,20 +172,95 @@ def generateAllWeights(nodeFile='defineNodes.json', linksFile='defineLinks.json'
 def generateWeights(pointA, pointB, maxX, maxY):
     x1, y1 = pointA
     x2, y2 = pointB
-    dist = math.sqrt((x2-x1)**2+(y2-y1)**2)
-    lamb = 0
+    dist = np.sqrt((x2-x1)**2+(y2-y1)**2)
+    lamb = 0.1 * dist/44
     dist += lamb
     # Fixed lambda vs Changing Lambda according to size
 
     mesh = np.mgrid[0:maxX,0:maxY]
     distX = mesh[0]
     distY = mesh[1]
-    distA = math.sqrt((distX - x1)**2 + (distY - y1)**2)
-    distB = math.sqrt((distX - x2)**2 + (distY - y2)**2)
+    distA = np.sqrt((distX - x1)**2 + (distY - y1)**2)
+    distB = np.sqrt((distX - x2)**2 + (distY - y2)**2)
     
-    grid = (distA+distB)>dist
+    grid = (distA+distB)<=dist
     return grid
+
+
+def generateAllWeights(nodeFile='defineNodes.json', linksFile='defineLinks.json', weightsFile='defineWeightsEll.json', delta=6.25):
+    # Grid Template
+    # grid, minX, maxX, minY, maxY = generateGrid(corners=1, ROI='b')
+    maxX, maxY = generateGrid()
+    nodes = json.load(open(nodeFile))
+    links = {}
+    linkCount = 0 
+    for thisNode in nodes['nodeList']:
+        # Identify possible links
+        possibleLinks = []
+        for checkNode in nodes['nodeList']:
+            # Avoid interference with original list
+            node = [thisNode[0], thisNode[1]]
+            otherNode = [checkNode[0], checkNode[1]]
+            # Conversion to Grid
+            node[0] = int(node[0]/delta)+(node[0]%delta>0)
+            node[1] = int(node[1]/delta)+(node[1]%delta>0)
+            otherNode[0] = int(otherNode[0]/delta)+(otherNode[0]%delta>0)
+            otherNode[1] = int(otherNode[1]/delta)+(otherNode[1]%delta>0)
+            
+            # Edge Cases
+            if (node[0]==otherNode[0]):
+                if (node[0]==maxY or node[0]==0):
+                    # Same Edge
+                    continue
+                elif (node[1]==otherNode[1]): 
+                    # Same Node
+                    continue
+            elif (node[1]==otherNode[1]):
+                if (node[1]==maxX or node[1]==0):
+                    # Same Edge
+                    continue
+            elif (otherNode in possibleLinks):
+                continue
+            
+            if (str(otherNode) not in links):
+                possibleLinks.append(otherNode)
+            elif (node not in links[str(otherNode)]):
+                possibleLinks.append(otherNode)
+            else: 
+                continue
+
+            # Generate Weights for this Link
+
+            # Can use pointer/reference to avoid sending grid in every function call
+            # weight = generateWeightsBres(np.zeros_like(grid), node, otherNode)
+            weight = generateWeights(node, otherNode, maxX, maxY)
+            # x*y array --> 1*xy array (concatenate all y|x for all x)
+            weightVector = weight.flatten()
+            # concatenate this weight array into the weightMatrix --> numLinks*xy array
+            if (linkCount == 0):
+                weightMatrix = weightVector
+            else:
+                weightMatrix = np.vstack((weightMatrix, weightVector))
+            
+            linkCount += 1
+
+        # Save the possible links to Links Dictionary
+        # Saving Links (incomplete) is not really useful though
+        links[str(node)] = possibleLinks
+    print(weightMatrix.shape)
+
+    # Save Links to JSON File for reference
+    with open(linksFile, 'w') as output:
+        output.write(json.dumps(links, indent=4))
+    # Save Weights to JSON File for reference
+    with open(weightsFile, 'w') as output:
+        output.write(json.dumps(weightMatrix.tolist(), indent=4))
+
+    return 1
+
 
 ##############################################################
 # To Test: 
-# print(generateAllWeights())
+print(generateAllWeights())
+
+# Lambda needs to be tuned 0 < lbd < 1
