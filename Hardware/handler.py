4d68c1f93495a3fc34804907570690f5ac25d497
Lee Kwun YIn <76862621+kyleeax@users.noreply.github.com>
2023-03-07 02:21:03 +0800

    Add files via upload

--- Hardware/handler.py
+++ Hardware/handler.py
@@ -1,28 +1,33 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler ,SimpleHTTPRequestHandler
 import simplejson
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # send 200 response
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def _set_headers(self):
         self.send_response(200)
-        # send response headers
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        # send the body of the response
-        self.wfile.write(bytes("It Works!", "utf-8"))
 
     def do_POST(self):
+        self._set_headers()
         # read the content-length header
         content_length = int(self.headers.get("Content-Length"))
         # read that many bytes from the body of the request
         body = self.rfile.read(content_length)
-        data =simplejson.loads(body)
-        with open("test.json","w") as outfile:
-            simplejson.dump(data,outfile)
-        print "{}".format(data)
+        with open("test.json", "r") as infile:
+            original_data = simplejson.load(infile)
+        new_data = simplejson.loads(body)
+        original_data.append(new_data)
+        with open("test.json", "w") as outfile:
+            simplejson.dump(original_data, outfile, indent=2)
+        print("{}".format(original_data))
+
         self.send_response(200)
         self.end_headers()
         # echo the body in the response
         self.wfile.write(body)
 
-httpd = HTTPServer(('localhost', 8000), MyHandler)
-httpd.serve_forever()
+
+host = ''
+port = 8000
+httpd = HTTPServer((host, port), MyHandler).serve_forever()
