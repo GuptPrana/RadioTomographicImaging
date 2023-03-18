a3148bddea02189661fa925c08e0210f5fed5783
Pranav Gupta <68948549+GuptPrana@users.noreply.github.com>
2023-03-19 01:49:05 +0800

    Updated Weights Method

--- Reconstruction/imaging.py
+++ Reconstruction/imaging.py
@@ -2,19 +2,22 @@ import json
 import numpy as np
 import nodeLinks as nl 
 import reconstructors
+from convertSimData import extractRSSI
 from matplotlib import pyplot as plt
 
-def generateImage(weightsFile='defineWeights.json', RSSI=[], resolution=33):
+def generateImage(weightsFile='defineWeightsEll.json', RSSI=[], resolution=32, lamb=0.01):
     weights = json.load(open(weightsFile))
     weights = np.array(weights)
     # print(weights.shape)
-    '''
+    
     # replace np.ones([130, 1]) with the RSSI values of same dimension
-    image = reconstructors.reconstructSVD(weights, np.ones([130, 1]))
+    rssi = np.array(RSSI) #np.ones(130)
+    image = reconstructors.reconstructSVD(weights, rssi, lamb)
     image = np.split(image, resolution)
     plt.imshow(image)
     plt.show()
     return
+    
     '''
     # The below is only for demo and can be removed
     # Change i in weights[i] to see different lines
@@ -23,5 +26,8 @@ def generateImage(weightsFile='defineWeights.json', RSSI=[], resolution=33):
     plt.imshow(w)
     plt.show()
     return
-    
-generateImage()
+    '''
+
+rssi = extractRSSI()
+# print(*rssiValues, sep=', ')
+generateImage(RSSI=rssi)
