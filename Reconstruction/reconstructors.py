b071010838e5fe53e4f6f60525cd8dabad9acd01
Pranav Gupta <68948549+GuptPrana@users.noreply.github.com>
2023-03-24 00:53:47 +0800

    Add files via upload

--- Reconstruction/reconstructors.py
+++ Reconstruction/reconstructors.py
@@ -15,25 +15,15 @@ def reconstructSVD(weights, rssi, lamb=0.1):
     U, S, Vt = np.linalg.svd(weights, full_matrices=False)
     # print(U.shape, S.shape, Vt.shape)
     # Inverse Problem: y = W*x --> x = W_inv*y
-    S_inv = np.diag(np.reciprocal(S)) 
+    # S_inv = np.diag(np.reciprocal(S)) 
+    S_inv = np.reciprocal(S)+lamb*np.ones(S.shape)
     # Ridge
-    S_inv = S_inv + lamb*np.ones(S_inv.shape)
+    S_inv = np.diag(S_inv)
     W_inv = np.dot(np.dot(Vt.T, S_inv), U.T)
     # print(W_inv.shape)
     return np.dot(W_inv, rssi)
 
 
-# def reconstructSVD(weights, rssi):
-#     # SVD of Weights
-#     weights = np.ones([130, 1089])
-#     # print(weights.shape)
-#     # weights = np.random.rand(130, 1089)
-#     # print(weights.shape)
-#     U, S, Vt = linalg.svds(weights, 10)
-#     # U, S, Vt = np.linalg.svd(weights, full_matrices=False)
-#     # print(U.shape, S.shape, Vt.shape)
-#     # Inverse Problem: y = W*x --> x = W_inv*y
-#     S_inv = np.diag(np.reciprocal(S))
-#     W_inv = np.dot(np.dot(Vt.T, S_inv), U.T)
-#     # print(W_inv.shape)
-#     return np.dot(W_inv, rssi)
+def reconstruct_updated(weights, rssi, res=32, lamb=0.1):
+    output = np.linalg.solve((np.matmul(weights.T, weights) + lamb*np.eye(res ** 2)),  (np.matmul(weights.T, rssi)))
+    return output
